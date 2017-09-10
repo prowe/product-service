9c7ae7b356372e97d3ab1265a08d20f9352a5cf6
Paul <paul.w.rowe@gmail.com>
2017-09-09 20:26:00 -0500

    enable cors

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const express = require('express');
 const data = require('./data.json');
 const PORT = 8080;
 
+function corsMiddleware(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}
+
 function listProducts(req, res) {
     console.log('listing products');
     res.json(data);
@@ -11,7 +17,7 @@ function getProductById(req, res) {
     let id = req.params.id;
     console.log('listing products by id ', id);
     let product = data.find(p => p.id == id);
-    
+
     if (product) {
         res.json(product);
     } else {
@@ -20,6 +26,7 @@ function getProductById(req, res) {
 }
 
 const app = express();
+app.use(corsMiddleware);
 app.get('/products', listProducts);
 app.get('/products/:id', getProductById);
 
