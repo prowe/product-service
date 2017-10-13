8a1f1dc7f7cc8863df51b1445a2b8970649ce419
Paul Rowe <prowe@sourceallies.com>
2017-10-12 20:05:33 -0500

    removing cart

--- main.js
+++ main.js
@@ -10,62 +10,22 @@ function corsMiddleware(req, res, next) {
     next();
 }
 
-function listProducts(req, res) {
+const app = express();
+app.use(corsMiddleware);
+app.use(bodyParser.json());
+
+app.get('/products', (req, res) => {
     console.log('listing products');
     res.json(data);
-}
+});
 
-function getProductById(req, res) {
+app.get('/products/:id', (req, res) => {
     let id = req.params.id;
     console.log('listing products by id ', id);
     let product = data.find(p => p.id == id);
 
     if (product) {
         res.json(product);
-    } else {
-        res.status(404).end();
-    }
-}
-
-const app = express();
-app.use(corsMiddleware);
-app.use(bodyParser.json());
-app.get('/products', listProducts);
-app.get('/products/:id', getProductById);
-
-let lineItemIds = 0;
-const shoppingCarts = {};
-
-app.post('/shopping-carts/:id/line-items', (req, res) => {
-    let id = req.params.id;
-    let cart = shoppingCarts[id];
-    if(!cart) {
-        cart = shoppingCarts[id] = {
-            lineItems: []
-        };
-    }
-
-    let lineItem = req.body;
-    lineItem.lineItemId = ++lineItemIds;
-    console.log('adding', lineItem);
-    cart.lineItems.push(lineItem);
-    res.json(cart);
-});
-
-app.get('/shopping-carts/:id', (req, res) => {
-    let id = req.params.id;
-    let cart = shoppingCarts[id];
-    res.json(cart);
-});
-
-app.delete('/shopping-carts/:id/line-items/:lineItemId', (req, res) => {
-    let id = req.params.id;
-    let lineItemId = req.params.lineItemId;
-    let cart = shoppingCarts[id];
-    console.log('deleting item ', lineItemId);
-    if(cart) {
-        cart.lineItems = cart.lineItems.filter(li => li.lineItemId != lineItemId)
-        res.json(cart);
     } else {
         res.status(404).end();
     }
